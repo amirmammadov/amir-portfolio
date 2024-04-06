4b8b7fa1958ee95c61865c8d7a788e06b0278b51
amirmammadov <amirmammado@gmail.com>
2024-04-06 17:59:23 +0400

    experience section was added

---
+++ data/experienceData.tsx
@@ -0,0 +1,20 @@
+export const experience = [
+  {
+    id: 1,
+    place: "MOTO4 B2B2C Global Startup",
+    job: "Frontend Developer",
+    date: "11/2023 - present",
+    image: "work/moto4.jpg",
+    link: "https://www.linkedin.com/company/motofour2023",
+    desc: "Currently, I work as a frontend developer for a startup called Moto4, and we are working remotely as a team on a car advertisements project. For this, we are using technologies such as React, TanStack Query, Material UI, and SASS",
+  },
+  {
+    id: 2,
+    place: "Coders Azerbaijan",
+    job: "Frontend Mentor",
+    date: "02/2024 - 06/2024",
+    image: "work/coders.jpg",
+    link: "https://coders.edu.az/",
+    desc: "As a mentor, my responsibilities include assigning projects and tasks to reinforce learning, as well as offering guidance on coding techniques and best practices to the team",
+  },
+];
